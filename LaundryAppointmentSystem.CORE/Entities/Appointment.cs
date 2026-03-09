35b49ecdf3064b4ea7ed1e0e33fb6c377125a7a3
iamteamstar <azadkocak29@gmail.com>
2026-03-09 11:59:14 +0300

    feat(start): entity classes was written

---
+++ LaundryAppointmentSystem.CORE/Entities/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundryAppointmentSystem.CORE.Entities
+{
+	public class Appointment
+	{
+		public int ID { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime FinishDate { get; set; }
+		public int StudentID { get; set; }
+		public Student Students { get; set; }
+		public int MachineID { get; set; }
+		public Machine Machine { get; set; }
+	}
+}
---
+++ LaundryAppointmentSystem.CORE/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundryAppointmentSystem.CORE.Entities
+{
+	public class Student
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string No { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public string Password { get; set; }
+		public List<Appointment> Appointments { get; set; }
+	}
+}
