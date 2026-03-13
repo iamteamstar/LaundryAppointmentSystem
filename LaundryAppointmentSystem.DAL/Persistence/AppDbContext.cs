71eb1b9e8a59bdd3678166d39935dae0ff7d0fbd
iamteamstar <azadkocak29@gmail.com>
2026-03-13 16:14:49 +0300

    feat(context): AppDbContext class was written

---
+++ LaundryAppointmentSystem.DAL/Persistence/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using LaundryAppointmentSystem.CORE.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundryAppointmentSystem.DAL.Persistence
+{
+	public class AppDbContext: DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+		}
+		DbSet<Student> Students{ get; set; }
+		DbSet<LoundryMachine> LoundryMachines { get; set; }
+		DbSet<Appointment> Appointments { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+
+	
+}
---
+++ LaundryAppointmentSystem.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundryAppointmentSystem.DAL.Repositories
+{
+	public class GenericRepository
+	{
+	}
+}
