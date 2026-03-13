ebb81e5d82ba9eb90f1ff5aa012dc64988c6c55b
iamteamstar <azadkocak29@gmail.com>
2026-03-13 16:16:31 +0300

    feat(IGenericRepository): crud methods interface was written

---
+++ LaundryAppointmentSystem.CORE/IServices/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundryAppointmentSystem.CORE.IServices
+{
+	public interface IGenericRepository<T>where T : class
+	{
+		//tüm verileri getiren metot
+		IQueryable<T> GetAll();
+		//id ye göre getiren metot
+		ValueTask<T> GetByIdAsync(int id);
+		ValueTask AddAsync(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+	}
+}
