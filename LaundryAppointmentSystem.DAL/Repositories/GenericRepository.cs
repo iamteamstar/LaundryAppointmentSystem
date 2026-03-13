2c97e7bd9ba19aaff1fedc1a051dae848c2ca8e1
iamteamstar <azadkocak29@gmail.com>
2026-03-13 16:36:12 +0300

    feat(GenericRepository): crud methods was written in repo class

--- LaundryAppointmentSystem.DAL/Repositories/GenericRepository.cs
+++ LaundryAppointmentSystem.DAL/Repositories/GenericRepository.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LaundryAppointmentSystem.CORE.IServices;
+using LaundryAppointmentSystem.DAL.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace LaundryAppointmentSystem.DAL.Repositories
 {
-	public class GenericRepository
+	public class GenericRepository<T> : IGenericRepository<T> where T : class
 	{
+		protected readonly AppDbContext _context;
+		private readonly DbSet<T> _dbSet;
+		public GenericRepository(AppDbContext context)
+		{
+			_context= context;
+			_dbSet= _context.Set<T>();//tablolara dinamik erişim(Araştır)
+		}
+		public async ValueTask AddAsync(T entity)
+		{
+		await _dbSet.AddAsync(entity);
+		}
+
+		public void Delete(T entity)
+		{
+		_dbSet.Remove(entity);
+		}
+
+		public IQueryable<T> GetAll()
+		{
+			return _dbSet.AsNoTracking().AsQueryable();//Performans için AsNoTracking eklendi
+		}
+
+		public async ValueTask<T> GetByIdAsync(int id)
+		{
+		return await _dbSet.FindAsync(id);//FirstOrDefault yerine FindAsync kullanman performansı artırır çünkü FindAsync önce bellekte (RAM'de) bu ID'ye sahip veri var mı diye bakar, yoksa veritabanına gider.
+		}
+
+		public void Update(T entity)
+		{
+			_context.Entry(entity).State=EntityState.Modified;//Bu, sadece değişen alanların güncellenmesini sağlayan, çok daha performanslı ve profesyonel bir yöntemdir.
+		}
 	}
 }
